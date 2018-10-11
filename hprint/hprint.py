57e9eec6432998db91bb485146e585c526d27318
Gabriel Rodrigues <gaborod16@gmail.com>
2018-10-11 20:59:22 +0100

    Add __init_py file Create class structure for the HLogger Create class methods to access and modify the class attribute: 'hierarchy' Add class level colors Define Color and Style Enums Add the logging functions to Logger Add multiline division for long texts respecting the hierarchy format Add 'log_operation' and 'decorate_log' to add hierarchy to function calls Add elapsed time to the 'log_operation' method Define Logger logging design Add write to log method

--- hprint/hprint.py
+++ hprint/hprint.py
@@ -1,35 +1,134 @@
-from termcolor import colored, cprint
+import timeit
+import termcolor
 
-import datetime
-import random
-import time
+from termcolor import colored
+from colorama import init
+from enum import Enum
 
-def getCurrentDate(format):
-	return datetime.datetime.now().strftime(format)
+class Color(Enum):
+	GREY = 'grey'
+	RED = 'red'
+	GREEN = 'green'
+	YELLOW = 'yellow'
+	BLUE = 'blue'
+	MAGENTA = 'magenta'
+	CYAN = 'cyan'
+	WHITE = 'white'
 
-hierarchy = 0
+class Style(Enum):
+	BOLD = ['bold']
+	UNDERLINED = ['underlined']
+	DARK = ['dark']
+	BLINK = ['blink']
+	REVERSE = ['reverse']
+	CONCEALED = ['concealed']
 
-## TODO Put on a class and own file for easier readability throughout the code
-def yprint(text, ptype='std'):
-	global hierarchy
+class Logger(object):
+	"""docstring for Logger"""
 
-	if ptype == 'std':
-		print(('\t'*hierarchy) + text)
-	elif ptype == 'w':
-		print(colored(('\t'*hierarchy) + 'Warning -> ' + text, 'yellow'))
-	elif ptype == 'e':
-		print(colored(('\t'*hierarchy) + 'Error -> ' + text, 'red'))
-	elif ptype == 'i':
-		print(colored(('\t'*hierarchy) + 'Info: ' + text, 'blue'))
-	elif ptype == 's':
-		print(colored(('\t'*hierarchy) + 'Success: ' + text, 'green'))
-	elif ptype == 'st':
-		print(colored(('\t'*hierarchy) + 'Task: ' + text, 'white', attrs=['bold']))
-		hierarchy = hierarchy + 1
-	elif ptype == 'dn':
-		hierarchy = hierarchy - 1
-		print(('\t'*hierarchy) + '----- Done: ' + text + ' -----\n')
-	elif ptype == 'h':
-		print(colored(('\t'*hierarchy) + text, 'magenta', attrs=['bold']))
-	else:
-		print(colored('Utils.yprint error! Bad usage!', 'red'))
+	hierarchy = 0
+
+	# Colors
+	colors = {}
+	colors['default'] = Color.WHITE.value
+	colors['err'] = Color.RED.value
+	colors['warn'] = Color.YELLOW.value
+	colors['info'] = Color.BLUE.value
+	colors['debug'] = Color.CYAN.value
+	colors['win'] = Color.GREEN.value
+	colors['begin'] = Color.WHITE.value
+	colors['end'] = Color.WHITE.value
+	colors['high'] = Color.MAGENTA.value
+
+	@classmethod
+	def raiseHierarchy(cls):
+		cls.hierarchy += 1
+
+	@classmethod
+	def dropHierarchy(cls):
+		cls.hierarchy -= 1
+
+	@classmethod
+	def getHierarchy(cls):
+		return cls.hierarchy
+
+	def __init__(self, verbose=True, filename=None):
+		super(Logger, self).__init__()
+		self.verbose = verbose
+		self.filename = filename
+		self.max_line_len = 80
+
+	def decorate_log(self, func):
+		def wrapper(*args, **kwargs):
+			return self.log_operation(func, *args, **kwargs)
+		return wrapper
+
+	def log_operation(self, func, *args, **kwargs):
+		self.begin(func.__name__)
+		start_time = timeit.default_timer()
+
+		results = func(*args, **kwargs)
+
+		elapsed_time = timeit.default_timer() - start_time
+		self.end('<{}> [Took: {}s]'.format(func.__name__, round(elapsed_time,2)))
+		return results
+
+	def _print(self, header='', message='', raw_text=''):
+		hierarchy_tabs = '\t'*Logger.getHierarchy()
+		self._write_log(hierarchy_tabs + raw_text)
+		outputs = [header + message]
+
+		while len(outputs[-1]) + len(hierarchy_tabs) > self.max_line_len:
+			outputs += self._split_text(outputs.pop())
+		[print('{}.{}'.format(hierarchy_tabs, o)) for o in outputs]
+
+	def _split_text(self, text):
+		''' This function turns a text multiline while respecting the hierarchy format '''
+		indexes = [i for i, v in enumerate(text) if v == ' ']
+		index = self._find_index(0, indexes)
+		return [text[:index], text[index+1:]]
+
+	def _find_index(self, iindex, indexes):
+		if indexes[iindex] <= self.max_line_len:
+			return self._find_index(iindex+1, indexes)
+		else:
+			return indexes[iindex-1]
+
+	def _write_log(self, text):
+		with open(self.filename, 'a+') as f:
+			f.write(text + '\n')
+
+	def begin(self, message):
+		message = '<{}>'.format(message)
+		header = 'Task: '
+		self._print(colored(header, Logger.colors['begin'], attrs=Style.BOLD.value), message, header+message)
+		Logger.raiseHierarchy()
+
+	def end(self, message):
+		Logger.dropHierarchy()
+		message = '{}\n'.format(message)
+		header = 'Done processing '
+		self._print(colored(header, Logger.colors['end']), message, header+message)
+
+	def error(self, message):
+		header = 'Error -> '
+		self._print(colored(header, Logger.colors['err']), message, header+message)
+
+	def warn(self, message):
+		header = 'Warning -> '
+		self._print(colored(header, Logger.colors['warn']), message, header+message)
+
+	def debug(self, message):
+		header = 'Debug: '
+		self._print(colored(header, Logger.colors['debug']), message, header+message)
+
+	def info(self, message):
+		header = 'Info: '
+		self._print(colored(header, Logger.colors['info']), message, header+message)
+
+	def win(self, message):
+		header = 'Success: '
+		self._print(colored(header, Logger.colors['win']), message, header+message)
+
+	def high(self, message):
+		self._print(message=colored(message, Logger.colors['high'], attrs=Style.BOLD.value), raw_text=message)
