91358a93de30a0bad9e0049ed150b3ad169ebf03
Gabriel Rodrigues <gaborod16@gmail.com>
2018-10-12 20:42:08 +0100

    Update functions_test and its captured image

--- hprint/tests/functions_test.py
+++ hprint/tests/functions_test.py
@@ -8,25 +8,6 @@ sys.path.append("..")
 from termcolor import colored
 from hprint import Logger
 
-def colors():
-	print(colored( 'This is a color', 'grey', attrs=[]))
-	print(colored( 'This is a color', 'red', attrs=[]))
-	print(colored( 'This is a color', 'green', attrs=[]))
-	print(colored( 'This is a color', 'yellow', attrs=[]))
-	print(colored( 'This is a color', 'blue', attrs=[]))
-	print(colored( 'This is a color', 'magenta', attrs=[]))
-	print(colored( 'This is a color', 'cyan', attrs=[]))
-	print(colored( 'This is a color', 'white', attrs=[]))
-
-
-def styles():
-	print(colored( 'This is a color', 'yellow', attrs=['bold']))
-	print(colored( 'This is a color', 'yellow', attrs=['underline']))
-	print(colored( 'This is a color', 'yellow', attrs=['dark']))
-	print(colored( 'This is a color', 'yellow', attrs=['concealed']))
-	print(colored( 'This is a color', 'yellow', attrs=['reverse']))
-	print(colored( 'This is a color', 'yellow', attrs=['blink']))
-
 def feedback(l):
 	l.info('This is a manual logged function')
 	l.warn('This is a very long warning message that will break into a multiline message to respect the hierarchy format.')
@@ -35,12 +16,16 @@ def feedback(l):
 if __name__ == '__main__':
 	logger = Logger(filename='ftest_1.log', verbose=False)
 
-	logger.error('Hello this is an error')
-	logger.warn('Hello this is a warning')
-	logger.high('Hello this is a highlight')
-	logger.info('Hello this is an information')
-	logger.debug('Hello this is an debug')
-	logger.win('Hello this is an win')
+	print('\n\n\nUsage: \n')
+
+	logger.debug('logger.debug()')
+	logger.info('logger.info()')
+	logger.high('logger.high()')
+	logger.warn('logger.warn()')
+	logger.error('logger.error()')
+	logger.win('logger.win()')
+
+	print('\n')
 	
 	logger.manual_log_func(feedback, logger)
 
