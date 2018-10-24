f5b29fda444e1004dc24f0775cb31e31d467a1f2
Gabriel Rodrigues <gaborod16@gmail.com>
2018-10-24 20:40:42 +0100

    Increase micro version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hclogger",
-    version="1.0.3",
+    version="1.0.4",
     author="Gabriel Amarista",
     author_email="gaborod16@gmail.com",
     description="A hierarchical and colourful logging module",
